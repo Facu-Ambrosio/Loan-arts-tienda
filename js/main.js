a63f79339e526ded7b62fd68d2c2cabcb9e6723b
Facundo Nahuel Ambrosio <facu.n.ambrosio@gmail.com>
2023-06-15 22:45:36 -0300

    la pagina tiene todas las funcionalidades

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ fetch("https://raw.githubusercontent.com/Facu-Ambrosio/proyectoFinalJS/main/data
   .then(response => response.json())
   .then(data => localStorage.setItem("productos", JSON.stringify(data)))
   .catch(error => console.log(error));
-
   
 const alertaNotificacion = (texto, color) => {
   color === "rojo" ? color = "#BF0603" : color = "#2EAC67" 
@@ -18,12 +17,56 @@ const alertaNotificacion = (texto, color) => {
   }).showToast();
 };
 
-const sacarCarrito = (e) => {
-  let nombre = e;
-  console.log(nombre);
+const renderPrecio = (etapa) => {
+  let carrito = JSON.parse(localStorage.getItem("carrito")); 
+  let total = carrito.reduce((acumulador, objeto) => acumulador + (objeto.precio * objeto.cantidad), 0); 
+  let ul = document.getElementById("canvasCarrito");
+  let li;
+
+  if (carrito.length === 0){
+    let liPrevio = document.getElementById("precioTotal");
+    if(liPrevio){
+      liPrevio.remove();
+    }
+    li = document.createElement("li")
+    li.id = "precioTotal";
+    li.classList.add("list-group-item", "precioCarrito", "align-items-center", "listItemsCarrito", "justify-content-center");  
+    li.innerHTML=`
+    el carrito esta vacio
+    `;
+  } else {
+    if(etapa === "inicio"){
+      li = document.createElement("li");
+      li.id = "precioTotal";
+      li.classList.add("list-group-item", "precioCarrito", "align-items-center", "listItemsCarrito", "justify-content-center");  
+      li.innerHTML=`
+      el precio total es de $${total}
+      `;
+    } else {
+      li = document.getElementById("precioTotal");
+      li.innerHTML=`
+      el precio total es de $${total}
+      `;
+    }
+  }
+  ul.append(li);
+}
+
+
+const sacarDelCarrito = (e) => {
+  let btn = e.target;
+  let id = e.target.id
+  let carrito = JSON.parse(localStorage.getItem("carrito")); 
+  let liABorrar = btn.parentNode.parentNode;
+  let nombre = id.replace(/_/g, " ");
+  let carritoSinProducto = carrito.filter((el) => el.nombre != nombre);
+  localStorage.setItem("carrito",JSON.stringify(carritoSinProducto));
+  liABorrar.remove();
+  renderPrecio("modificacion");
 };
 
 
+// hacer aparecer los elementos en el carrito
 const renderCarrito = (objeto, operacion) => {
   let ul = document.getElementById("canvasCarrito");
   let nombre = objeto.nombre;
@@ -37,8 +80,7 @@ const renderCarrito = (objeto, operacion) => {
     li.id = `${nombre.replace(/ /g, "_")}_li` 
   } else if(operacion === "modificar") {
     li = document.getElementById(`${nombre.replace(/ /g, "_")}_li`);
-  } else if(operacion === "sacar"){}
-
+  } 
   li.innerHTML=`
   <div class = "divCarritoImagen col-2">
     <img src="../Assets/Galeria/reescaladas/${nombre}.jpg" class = "imagenCarrito">
@@ -50,27 +92,18 @@ const renderCarrito = (objeto, operacion) => {
     Precio: $ ${cantidad * precio}
   </div>
   <div class = "col-2">
-    <button type="button" class="btn btn-danger" id = "${nombre.replace(/ /g, "_")}_btn">
+    <button type="button" class="btn btn-danger" id = "${nombre.replace(/ /g, "_")}">
       Sacar del carrito
     </button>
   </div>
   `;
   ul.append(li);
 
-  
+  let btn = document.getElementById(`${nombre.replace(/ /g, "_")}`);
+  btn.addEventListener("click", sacarDelCarrito);
 };
 
-const carritoRenderTotal = () => {
-  if (!carrito){
-    carrito = [];
-    localStorage.setItem("carrito",JSON.stringify(carrito));
-  } else {
-    for (let i of carrito) {
-      renderCarrito(i, "crear");
-    }
-  }
-};
-
+// agarra los productos y los mete en el carrito, o les aumenta la cantidad
 const agregarAlCarritoStorage = (e) => {
   let nombre = e.target.id;
   let objeto = productos.find((el) => el.nombre === nombre);
@@ -81,15 +114,33 @@ const agregarAlCarritoStorage = (e) => {
   if (!objetoEnCarrito){
     objetoEnCarrito = objeto;
     carrito.push(objetoEnCarrito);
+    localStorage.setItem("carrito",JSON.stringify(carrito));
     renderCarrito(objetoEnCarrito, "crear");
+    renderPrecio("modificacion");
+    // funcion que cree el li pero con le parametro vacio
   } else {
     objetoEnCarrito.cantidad++;
+    localStorage.setItem("carrito",JSON.stringify(carrito));
     renderCarrito(objetoEnCarrito, "modificar");
+    renderPrecio("modificar");
+    // funcion que cree el li pero con los elementos del carrito
   }
   alertaNotificacion(`Has Agregado ${objetoEnCarrito.cantidad} producto/s de "${nombre}" al Carrito!!`, "verde" )
-  localStorage.setItem("carrito",JSON.stringify(carrito));
 };
 
+// cuando se inicia la pagina y se inicializa el carrito, so se hace vacio, o se renderiza todos los elementos del carrito
+const renderCarritoTotal = () => {
+  if (!carrito){
+    carrito = [];
+    localStorage.setItem("carrito",JSON.stringify(carrito));
+
+  } else {
+    for (let i of carrito) {
+      renderCarrito(i, "crear");
+    }
+  }
+  renderPrecio("inicio");
+};
 
 const renderTotal = () => { 
   let section = document.getElementById("sectionInicio"); 
@@ -111,9 +162,8 @@ const renderTotal = () => {
 };
 
 
-let carrito = JSON.parse(localStorage.getItem("carrito")); 
-carritoRenderTotal();
 let productos = JSON.parse(localStorage.getItem("productos")); 
 renderTotal(); 
-
+let carrito = JSON.parse(localStorage.getItem("carrito")); 
+renderCarritoTotal();
 
